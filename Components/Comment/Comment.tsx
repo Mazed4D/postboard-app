073ac6c6be81be816efc12af8dbd907b4728408d
Milan Paunovic <paunovic.milan@outlook.com>
2022-03-20 19:19:28 +0100

    added comment props

--- Components/Comment/Comment.tsx
+++ Components/Comment/Comment.tsx
@@ -1,6 +1,13 @@
 import { View, Text } from 'react-native';
 import React from 'react';
 
+type CommentProps = {
+	userId: string;
+	userName: string;
+	avatarUri: string;
+	isOwnComment: boolean;
+};
+
 const Comment = () => {
 	return (
 		<View>
