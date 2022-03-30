9f6de2f902cb7e2e442b75b83fb5c0cdd23b202c
Milan Paunovic <paunovic.milan@outlook.com>
2022-03-30 22:06:18 +0200

    removed dotenv import

--- api/api.service.ts
+++ api/api.service.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import * as SecureStore from 'expo-secure-store';
-import 'dotenv/config';
 
 const config = async () => {
 	const token = await SecureStore.getItemAsync('token');
--- api/auth.service.ts
+++ api/auth.service.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import * as SecureStore from 'expo-secure-store';
-import 'dotenv/config';
 
 type loginProps = {
 	email: string;
